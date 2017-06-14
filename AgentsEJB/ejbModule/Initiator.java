ebfc8ca9488c99d3c791b175fc39e0d4a32608bc
Ademi Misel <ademimisel@gmail.com>
2017-06-15 01:36:28 +0200

    Contract net fixing example

--- AgentsEJB/ejbModule/Initiator.java
+++ AgentsEJB/ejbModule/Initiator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateful;
 
@@ -16,6 +18,13 @@ public class Initiator extends Agent {
 	private static final long serialVersionUID = 1L;
 	private int pendingProposals;
 	private static final int NUM_PARTICIPANTS = 16;
+	
+	private int NUMBER_OF_SLAVES = 5;
+	private ArrayList<AID> rejectAids = new ArrayList<>();
+    private AID acceptAid = null;
+    private int bestBid = 1001;
+    private int REFUSED = 0;
+    private int PROPOSED = 0;
 
 	public Initiator(){
 		super();
@@ -38,12 +47,49 @@ public class Initiator extends Agent {
 			mbr.sendMessage(message);
 			pendingProposals = participants.length;
 			break;
-		case ACCEPT_PROPOSAL:
-			Container.getInstance().log("[ACCEPT_PROPOSAL]Initiator has received a message: "/* + msg*/);
+		case REFUSE:
+			REFUSED++;
+			String senderName = msg.getSender().getName();
+			Container.getInstance().log("Refuse to ContractNetMaster from " + senderName + " : " + msg.getContent());
+			
+			if(REFUSED + PROPOSED == NUM_PARTICIPANTS) {
+				deadline();
+			}
+			
+			/*Container.getInstance().log("[ACCEPT_PROPOSAL]Initiator has received a message: );
 			--pendingProposals;
 			if(pendingProposals == 0){
 				Container.getInstance().log("All proposals have been accepted!");
+			}*/
+			break;
+		case PROPOSE:
+			PROPOSED++;
+			String senderName2 = msg.getSender().getName();
+			Container.getInstance().log("Propose to ContractNetMaster from " + senderName2 + " : " + msg.getContent());
+			int bid = Integer.parseInt(msg.getContent());
+			
+			if(bid < bestBid) {
+				bestBid = bid;
+				if(acceptAid != null) {
+					rejectAids.add(acceptAid);
+				}
+				acceptAid = msg.getSender();
+			}else {
+				rejectAids.add(msg.getSender());
 			}
+			
+			if(REFUSED + PROPOSED == NUM_PARTICIPANTS) {
+				deadline();
+			}
+			
+			break;
+		case FAILURE:
+			 senderName = msg.getSender().getName();
+			 Container.getInstance().log(senderName + " failed to finish: " + msg.getContent());
+			break;
+		case INFORM:
+			senderName = msg.getSender().getName();
+			Container.getInstance().log(senderName + " finished successfully: " + msg.getContent());
 			break;
 		default:
 			Container.getInstance().log("Message not understood " /*+ msg*/);
@@ -61,6 +107,39 @@ public class Initiator extends Agent {
 		}
 
 		return participants;
+	}
+	
+	private void deadline() {
+		if(acceptAid == null) {
+			Container.getInstance().log("No proposals");
+		}else {
+			sendReject();
+			sendAccept();
+		}
+	}
+	
+	private void sendReject() {
+		if (rejectAids.size() > 0) {
+			ACLMessage reject = new ACLMessage();
+			reject.setPerformative(Performative.REJECT_PROPOSAL);
+			reject.setSender(getId());
+			for(AID raid : rejectAids) {
+				reject.addReceiver(raid);
+			}
+			reject.setContent("Master rejects Proposal!");
+			MessageBeanRemote mbr = findMB();
+			mbr.sendMessage(reject);
+		}
+	}
+
+	private void sendAccept() {
+		ACLMessage accept = new ACLMessage();
+		accept.setPerformative(Performative.ACCEPT_PROPOSAL);
+		accept.setSender(getId());
+		accept.addReceiver(acceptAid);
+		accept.setContent("Master accepts Proposal!");
+		MessageBeanRemote mbr = findMB();
+		mbr.sendMessage(accept);
 	}
 
 }
--- AgentsEJB/ejbModule/Participant.java
+++ AgentsEJB/ejbModule/Participant.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateful;
 
@@ -25,12 +27,59 @@ public class Participant extends Agent {
 
 	@Override
 	public void handleMessage(ACLMessage msg){
-		Container.getInstance().log(getId().getName() + " has received a message: "/* + msg*/);
-		ACLMessage reply = new ACLMessage(Performative.ACCEPT_PROPOSAL);
-		reply.addReceiver(msg.getReplyTo()!=null? msg.getReplyTo(): msg.getSender());
-		reply.setProtocol("ContractNet");
-		MessageBeanRemote mbr = findMB();
-		mbr.sendMessage(reply);
+		switch(msg.getPerformative()){
+		case CALL_FOR_PROPOSAL:
+			Container.getInstance().log(getId().getName() + " has received a message: "/* + msg*/);
+			
+			Random random = new Random();
+			int percentage = random.nextInt(100);
+			
+			ACLMessage reply = new ACLMessage();
+			reply.setSender(getId());
+			reply.addReceiver(msg.getReplyTo()!=null? msg.getReplyTo(): msg.getSender());
+			
+			if(percentage < 50) {
+				reply.setPerformative(Performative.REFUSE);
+				reply.setProtocol("ContractNet");
+				reply.setContent("I do not want to make a bid!");
+			}else {
+				int bid = random.nextInt(1000);
+				reply.setPerformative(Performative.PROPOSE);
+				reply.setContent(Integer.toString(bid));
+			}
+			
+			MessageBeanRemote mbr = findMB();
+			mbr.sendMessage(reply);
+			break;
+		case ACCEPT_PROPOSAL:
+			String receiverName = getId().getName();
+			Container.getInstance().log("Accept Proposal to " + receiverName + ": " + msg.getContent());
+
+			random = new Random();
+			percentage = random.nextInt(100);
+
+			reply = new ACLMessage();
+			reply.setSender(getId());
+			reply.addReceiver(msg.getSender());
+
+			if (percentage < 10) {
+				// failure
+				reply.setPerformative(Performative.FAILURE);
+				reply.setContent("Failed!");
+			} else {
+				// inform
+				reply.setPerformative(Performative.INFORM);
+				reply.setContent("Completed!");
+			}
+			
+			mbr = findMB();
+			mbr.sendMessage(reply);
+			break;
+		case REJECT_PROPOSAL:
+			receiverName = getId().getName();
+			Container.getInstance().log("Reject Proposal to " + receiverName + ": " + msg.getContent());
+			break;
+		}
 	}
 
 }
